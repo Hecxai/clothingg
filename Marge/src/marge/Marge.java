d409e70d8ea5514f15100c49d2c2cd263685de2f
Hecxai <marioreyesv@gmail.com>
2022-03-10 10:09:04 -0600

    Ejercicio Marge

---
+++ Marge/src/marge/Marge.java
@@ -0,0 +1,15 @@
+
+package marge;
+
+
+public class Marge {
+
+
+    public static void main(String[] args) {
+                                                              
+              
+        
+        
+    }
+    
+}
