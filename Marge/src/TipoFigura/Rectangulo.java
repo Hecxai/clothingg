5094f26689e90d512071379db3c8da26629b02a3
Hecxai <marioreyesv@gmail.com>
2022-03-10 10:35:37 -0600

    Actualización Clase TipoFigura

--- Marge/src/TipoFigura/Rectangulo.java
+++ Marge/src/TipoFigura/Rectangulo.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package TipoFigura;
 
-/**
- *
- * @author labtw06
- */
-public class Rectangulo {
+public class Rectangulo{
     
 }
